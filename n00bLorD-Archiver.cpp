6683d4852cf1e3abbdc8c5512c07e897fefcdfb8
ddulshan <ddulshan100@gmail.com>
2018-09-04 00:16:13 +0530

    Add files via upload

--- n00bLorD-Archiver.cpp
+++ n00bLorD-Archiver.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <time.h>
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <iomanip>
+#include <stdlib.h>
 
 using namespace std;
+
+struct header {
+    const float version = 1.00;
+    unsigned int files_no;
+};
+
+struct entry {
+    unsigned int id;
+    char *file_name;
+    unsigned int file_size;
+};
 
 int usage(string fileName) {
     cerr << "\nUsage option, \n\n"
@@ -13,49 +26,58 @@ int usage(string fileName) {
          << "-v,--view\tView file(s) in an archive file\n"
          << "-h,--help\tShow usage information\n";
 
-         return 0;
+    return 0;
 }
 
-int archive(string files[], string output, int file_len) {
+int archive(char *argv[], int argc) {
     FILE *read_ptr, *write_ptr;
     size_t result;
-    long file_size;
+    int file_size;
     char *buffer;
     char entry_text[8] = "###=###";
 
-    for(int c = 0; c < file_len; c++) {
-        read_ptr = fopen(files[c].c_str(), "rb");
+    string output = argv[argc-1]; output += ".n0b";
+
+    cout << "\nArchiving files into Archive : " << output << endl;
+
+    for(int i = 2; i < argc-1; i++) {
+        read_ptr = fopen(argv[i], "rb");
         if(read_ptr == NULL) {
-            cout << files[c] << " : File not found\n";
+            cout << argv[i] << " : File not found\n";
         }
         else {
-            cout << files[c] << " : File found\n";
-
             write_ptr = fopen(output.c_str(), "ab");
+            string file_name = argv[2];
+            const size_t last_slash = file_name.find_last_of("\\/");
+            if(std::string::npos != last_slash)
+                file_name.erase(0, last_slash + 1);
 
             fseek(read_ptr, 0, SEEK_END);
             file_size = ftell(read_ptr);
             rewind(read_ptr);
 
             buffer = (char*) malloc (sizeof(char)*file_size);
-            result = fread(buffer, 1, file_size, read_ptr);
+            if((result = fread(buffer, 1, file_size, read_ptr)) != file_size) {
+                cout << argv[i] << " : File read error\n";
+                continue;
+            }
 
-            fprintf(write_ptr, "%s %s %d ", entry_text, files[c].c_str(), file_size);
+            fprintf(write_ptr, "%s %s %d ", entry_text, file_name.c_str(), file_size);
             fwrite(buffer, 1, file_size, write_ptr);
 
             fclose(write_ptr);
+            cout << argv[i] << " : Done\n";
         }
     }
-
-    return 0;
 }
 
 int view(string archive_name) {
     FILE *read_ptr;
     char *buffer, *line;
-    string file_name, entry;
-    float file_size;
+    char *file_name, *entry;
+    float file_size, file_size_readable;
     int offset;
+
     read_ptr = fopen(archive_name.c_str(), "rb");
 
     if(read_ptr == NULL) {
@@ -65,63 +87,108 @@ int view(string archive_name) {
         cout << "\nFiles in archive : " << archive_name << ",\n\n"
              << "Filename\t\tSize\n-------------------------------------------\n";
 
-        for(int c = 0; c < 5; c++){
-            fscanf(read_ptr, "%s %s %f ", &entry, &file_name, &file_size);
-            buffer = (char*) malloc (sizeof(char)*file_size);
+        do{
+            file_name = (char*) malloc(sizeof(char)*128);
+            entry = (char*) malloc(sizeof(char)*10);
+
+            fscanf(read_ptr, "%s %s %f ", entry, file_name, &file_size);
+
+            if(file_size > 1024*1024) {
+                file_size_readable = (file_size/1024)/1024;
+                cout << file_name << "\t\t" << fixed << setprecision(2) << file_size_readable << "MB\n";
+            }
+            else if(file_size > 1024) {
+                file_size_readable = file_size/1024;
+                cout << file_name << "\t\t" << fixed << setprecision(2) << file_size_readable << "KB\n";
+            }
+            else {
+                file_size_readable = file_size;
+                cout << file_name << "\t\t" << fixed << setprecision(2) << file_size_readable << "B\n";
+            }
+
+            buffer = (char*) malloc(sizeof(char)*file_size);
             size_t result = fread(buffer, 1, file_size, read_ptr);
 
-            float file_size_mb = (file_size/1024)/1024;
-            printf("%s\t\t%.2fMB\n",file_name, file_size_mb);
-
-            //FILE *write_ptr = fopen("test.mp4", "wb");
-            //fwrite(buffer, 1, file_sizes[0], write_ptr);
-            //fclose(write_ptr);
-        }
+            free(buffer);
+            free(file_name);
+            free(entry);
+        }while(fgetc(read_ptr) != EOF);
         fclose(read_ptr);
     }
+
+    return 0;
+}
+
+int restore(string archive_name) {
+    FILE *read_ptr, *write_ptr;
+    char *buffer, *file_name, *entry;
+    float file_size;
+    size_t result;
+
+    file_name = (char*) malloc(sizeof(char)*128);
+    entry = (char*) malloc(sizeof(char)*10);
+
+    read_ptr = fopen(archive_name.c_str(), "rb");
+    if(read_ptr == NULL) {
+        cout << "\n" << archive_name << " : Archive file not found\n";
+    }
+    else {
+        cout << "\nExtracting Archive : " << archive_name << "\n" << endl;
+        do {
+            fscanf(read_ptr, "%s %s %f ", entry, file_name, &file_size);
+
+            buffer = (char*) malloc(sizeof(char)*file_size);
+            result = fread(buffer, 1, file_size, read_ptr);
+
+            write_ptr = fopen(file_name, "wb");
+            fwrite(buffer, 1, file_size, write_ptr);
+            fclose(write_ptr);
+
+            cout << file_name << " : Done\n";
+        }while(fgetc(read_ptr) != EOF);
+        fclose(read_ptr);
+    }
+
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    string fileList[128];
+    string archive_name = argv[argc-1];
+    string option = argv[1];
 
-    for(int c = 0; c <= argc; c++) {
-        string s_arg = argv[c];
-        if(s_arg == "-h" || s_arg == "--help" || argc == 1) {
-            usage(argv[0]);
-            break;
+    if(option == "-h" || option == "--help" || argc == 1) {
+        usage(argv[0]);
+    }
+    else if(option == "-a" || option == "--archive") {
+        if(argc >= 4) {
+            archive(argv, argc);
         }
-        else if(s_arg == "-a" || s_arg == "--archive") {
-            if(argc >= 4) {
-                cout << "\nArchiving files,\n";
-                string output = argv[argc-1]; output += ".n0b";
-                int x = 0;
-                for(int i = c+1; i < argc-1; i++) {
-                    fileList[x] = argv[i];
-                    cout << "> " << fileList[x] << "\n";
-                    x++;
-                }
-                cout << "\nInto Archive > " << output << "\n\n";
-
-                archive(fileList, output, argc-3);
-            }
-            else {
-                cerr << "\nOption -a,--archive requires atleast 2 arguments\n"
-                     << "Usage : " << argv[0] << " <-a|--archive> <file(s)> <archive>\n";
-            }
-
-            break;
+        else {
+            cerr << "\nOption -a,--archive requires atleast 2 arguments\n"
+                 << "Usage : " << argv[0] << " <-a|--archive> <file(s)> <archive>\n";
         }
-        else if(s_arg == "-v" || s_arg == "--view") {
-            if(argc == 3) {
-                string archive_name = argv[c+1];
-                view(archive_name);
-            }
-            else {
-                cerr << "\nOption -v,--view requires 1 argument\n"
-                     << "Usage : " << argv[0] << " <-v|--view> <archive>\n";
-            }
-            break;
+    }
+    else if(option == "-v" || option == "--view") {
+        if(argc == 3) {
+            view(archive_name);
         }
+        else {
+            cerr << "\nOption -v,--view requires 1 argument\n"
+                 << "Usage : " << argv[0] << " <-v|--view> <archive>\n";
+        }
+    }
+    else if(option == "-r" || option == "--restore") {
+        if(argc == 3) {
+            restore(archive_name);
+        }
+        else {
+            cerr << "\nOption -r,--restore requires 1 argument\n"
+                 << "Usage : " << argv[0] << " <-r|--restore> <archive>\n";
+        }
+    }
+    else {
+        cout << "\nInvalid usage,\n";
+        usage(argv[0]);
     }
 
     return 0;

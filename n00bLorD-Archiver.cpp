4493dfdd02949b8d8f22b77852bf7a327759cb4e
ddulshan <ddulshan100@gmail.com>
2018-09-06 19:37:34 +0530

    Add files via upload

--- n00bLorD-Archiver.cpp
+++ n00bLorD-Archiver.cpp
@@ -6,8 +6,11 @@
 #include <iomanip>
 #include <sys/stat.h>
 #include <sstream>
+#include <dirent.h>
 
 using namespace std;
+
+#define CHUNK_SIZE 67108864
 
 struct archiveHeader {
     char    version[5];     //Offset 0, length 5, Archiver version
@@ -15,7 +18,7 @@ struct archiveHeader {
     char    desc[256];      //Offset 13, length 256, Archive description, Header size 296?
 
     archiveHeader() {
-        strcpy(version, "0.21");
+        strcpy(version, "0.25");
         strcpy(desc, "THIS IS A DESCRIPTION FIELD WITH 256BYTE!");
         time(&time_created);
     }
@@ -27,10 +30,11 @@ struct fileEntry {
     int     file_size;      //Offset 132, length 64, File size in bytes
     int     chunk_size;     //Offset 196, length 4, Size in bytes of single chunk
     int     chunks;         //Offset 200, length 8, Number of chunks of chunk_size bytes
+    int     chunk_rem;
     time_t  time_added;     //Offset 208, length 4, Time object created, Header Size 212?
 
     fileEntry() {
-        chunk_size = 512;
+        chunk_size = CHUNK_SIZE;
         time(&time_added);
     }
 };
@@ -41,8 +45,6 @@ int usage() {
          << "-r,--restore\tRestore file(s) from archive\n"
          << "-v,--view\tView file(s) in an archive file\n"
          << "-h,--help\tShow usage information\n";
-
-    archiveHeader head;
 
     return 0;
 }
@@ -56,7 +58,8 @@ int archive(char *argv[], int argc) {
     int             file_size, write_counter = 1;
     bool            counter_updated = false;
 
-    cout << "\nArchiving files into Archive > " << output << "\n\n";
+
+    cout << "\nArchiving file(s) into Archive > " << output << "\n\n";
 
     for(int i = 2; i < argc-1; i++) {
         ifstream file_read(argv[i], ios::binary);
@@ -68,7 +71,6 @@ int archive(char *argv[], int argc) {
             struct stat check_buff;
             file_name = argv[i];
             if(stat(output.c_str(), &check_buff) == 0 && !counter_updated) {
-                cout << "FILE FOUND! NOT UPDATED";
                 ifstream archive_read(output.c_str(), ios::binary);
                 if(archive_read.fail()) {
                     cerr << "\n" << output << " : Archive read error" << endl;
@@ -77,33 +79,31 @@ int archive(char *argv[], int argc) {
                 }
                 else {
                     archive_read.read((char*)&archive_struct_read, sizeof(archive_struct_read));
-                    do{
-                        archive_read.read((char*)&file_struct_read, sizeof(file_struct_read));
-                        archive_read.ignore(file_struct_read.file_size);
+                    while(archive_read.read((char*)&file_struct_read, sizeof(file_struct_read))){
+                        archive_read.seekg(archive_read.tellg() + file_struct_read.file_size);
 
-                        write_counter = file_struct_read.id;
-                    }while(!archive_read.eof());
+                        write_counter = file_struct_read.id + 1;
+                    }
                     archive_read.close();
                 }
                 counter_updated = true;
             }
             else if(stat(output.c_str(), &check_buff) == -1) {
-                cout << "NO FILE! WRITE HEADER" << endl;
                 ofstream archive_write(output.c_str(), ios::binary);
 
                 archive_write.write((char*)&archive_struct_write, sizeof(archive_struct_write));
                 archive_write.close();
             }
-            cout << "FILE WRITE START!" << endl;
+
             ofstream archive_write(output.c_str(), ios::binary | ios::app);
 
             file_read.seekg(0, file_read.end);
             file_size = file_read.tellg();
             file_read.seekg(0, file_read.beg);
 
-            buffer = (char*)malloc(sizeof(char)*file_size);
-            file_read.read(buffer, file_size);
-            file_read.close();
+            int chunk_num = file_size/CHUNK_SIZE;
+            int chunk_rem = file_size%CHUNK_SIZE;
+            int size_written = 0;
 
             const size_t last_slash = file_name.find_last_of("\\/");
             if(std::string::npos != last_slash)
@@ -113,11 +113,35 @@ int archive(char *argv[], int argc) {
 
             file_struct_write.file_size = file_size;
             strcpy(file_struct_write.file_name, file_name.c_str());
-            file_struct_write.chunks = 1;
             file_struct_write.id = write_counter;
+            file_struct_write.chunks = chunk_num;
+            file_struct_write.chunk_rem = chunk_rem;
 
             archive_write.write((char*)&file_struct_write, sizeof(file_struct_write));
-            archive_write.write(buffer, file_size);
+
+            if(chunk_num == 0 && chunk_rem != 0) {
+                buffer = (char*)malloc(sizeof(char)*chunk_rem);
+                file_read.read(buffer, chunk_rem);
+                archive_write.write(buffer, chunk_rem);
+                size_written += chunk_rem;
+                free(buffer);
+            }
+            else {
+                for(int c = 1; c <= chunk_num; c++) {
+                    buffer = (char*)malloc(sizeof(char)*CHUNK_SIZE);
+                    file_read.read(buffer, CHUNK_SIZE);
+                    archive_write.write(buffer, CHUNK_SIZE);
+                    size_written += CHUNK_SIZE;
+                    if(c == chunk_num) {
+                        file_read.read(buffer, chunk_rem);
+                        archive_write.write(buffer, chunk_rem);
+                        size_written += chunk_rem;
+                    }
+                    cout << setprecision(4) << (float)((size_written/(float)file_size)*(float)100) << "%" << endl;
+                    free(buffer);
+                }
+                file_read.close();
+            }
 
             if(archive_write.bad()) {
                 cout << "\n" << output << " : Archive write error" << endl;
@@ -147,22 +171,24 @@ int view(string archive_name) {
         cerr << "\n" << archive_name << " : Archive file not found\n";
     }
     else {
+        archive_read.read((char*)&archive_struct_read, sizeof(archive_struct_read));
+        if(strcmp(archive_struct_read.desc, "THIS IS A DESCRIPTION FIELD WITH 256BYTE!") != 0){
+            cout << "\n" << archive_name << " : Corrupted or unknown file type\n";
+            return -1;
+        }
+
         cout << "\nFiles in archive : " << archive_name << ",\n\n"
              << setw(8) << left << "#"
-             << setw(40) << left << "Filename"
-             << setw(10) << left << "Size"
-             << setw(20) << left << "Last Modified\n--------------------------------------------------------------------------------\n";
-        archive_read.read((char*)&archive_struct_read, sizeof(archive_struct_read));
+             << setw(30) << left << "Filename"
+             << setw(15) << right << "Size"
+             << setw(25) << right << "Last Modified" << endl
+             << "--------------------------------------------------------------------------------\n";
 
-        while(1) {
-            archive_read.read((char*)&file_struct_read, sizeof(file_struct_read));
-            buffer = (char*)malloc(sizeof(char)*file_struct_read.file_size);
-            archive_read.read(buffer, file_struct_read.file_size);
+        while(archive_read.read((char*)&file_struct_read, sizeof(file_struct_read))) {
+
+            archive_read.seekg(archive_read.tellg()+file_struct_read.file_size);
 
             time_struct = localtime(&file_struct_read.time_added);
-
-            if(archive_read.eof())
-                break;
 
             stream << time_struct->tm_hour << ":"
                    << time_struct->tm_min << ":"
@@ -171,12 +197,13 @@ int view(string archive_name) {
                    << time_struct->tm_mon + 1 << "-"
                    << time_struct->tm_year + 1900;
 
-            cout << setw(8) << left << file_struct_read.id
-                 << setw(40) << left << file_struct_read.file_name
-                 << setw(10) << left << file_struct_read.file_size
-                 << setw(20) << left << stream.str() <<endl;
+            float file_size_readable = (float)((file_struct_read.file_size/1024)/1024)/1024;
 
-            free(buffer);
+            cout << setw(8) << left << file_struct_read.id
+                 << setw(30) << left << file_struct_read.file_name
+                 << setw(15) << right << file_struct_read.file_size
+                 << setw(25) << right << stream.str() << endl;
+
             stream.str(string());
             stream.clear();
         }
@@ -188,26 +215,45 @@ int view(string archive_name) {
 int restore(string archive_name) {
     archiveHeader   archive_struct_read;
     fileEntry       file_struct_read, file_struct_write;
+    char            *buffer;
+    int             size_written = 0;
 
     ifstream archive_read(archive_name.c_str(), ios::binary);
 
     archive_read.read((char*)&archive_struct_read, sizeof(archive_struct_read));
 
-    while(1) {
-        archive_read.read((char*)&file_struct_read, sizeof(file_struct_read));
-
-        char * readbuff = new char [file_struct_read.file_size];
-        archive_read.read(readbuff, file_struct_read.file_size);
-
-        if(archive_read.eof())
-                break;
+    while(archive_read.read((char*)&file_struct_read, sizeof(file_struct_read))) {
 
         ofstream file_write(file_struct_read.file_name, ios::binary);
-        file_write.write(readbuff, file_struct_read.file_size);
 
+        for(int c = 0; c <= file_struct_read.chunks; c++) {
+
+            if(file_struct_read.chunks == 0 && size_written <= file_struct_read.chunk_rem) {
+                buffer = new char[file_struct_read.chunk_rem];
+                archive_read.read(buffer, file_struct_read.chunk_rem);
+                file_write.write(buffer, file_struct_read.chunk_rem);
+                size_written += file_struct_read.chunk_rem;
+            }
+            else if(file_struct_read.chunks == c+1 && size_written <= file_struct_read.chunk_rem) {
+                buffer = new char[file_struct_read.chunk_rem];
+                archive_read.read(buffer, file_struct_read.chunk_rem);
+                file_write.write(buffer, file_struct_read.chunk_rem);
+                size_written += file_struct_read.chunk_rem;
+                c++;
+            }
+            else {
+                buffer = new char[CHUNK_SIZE];
+                archive_read.read(buffer, CHUNK_SIZE);
+                file_write.write(buffer, CHUNK_SIZE);
+                size_written += CHUNK_SIZE;
+            }
+
+            free(buffer);
+            cout << (float)((float)size_written/(float)file_struct_read.file_size)*100 << "%" <<endl;
+        }
         file_write.close();
-        free(readbuff);
     }
+    archive_read.close();
 
     return 0;
 }
@@ -215,7 +261,7 @@ int restore(string archive_name) {
 int main(int argc, char *argv[]) {
 
     if(argc < 2) {
-        cerr << "\nInvalid usage,\n";
+        cerr << "\nInvalid usage\n";
         usage();
         return 0;
     }
